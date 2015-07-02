bab6519d4b60d5c5f78e97513279d841396de609
boyapati ravi kumar <boyravikumar@gmail.com>
2015-07-02 10:42:17 +0530

    Initial Commit

---
+++ Cube/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cube
+//
+//  Created by boyapati ravi kumar on 02/07/15.
+//  Copyright (c) 2015 Custom Furniish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CubeTests/CubeTests.m
@@ -0,0 +1,40 @@
+//
+//  CubeTests.m
+//  CubeTests
+//
+//  Created by boyapati ravi kumar on 02/07/15.
+//  Copyright (c) 2015 Custom Furniish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CubeTests : XCTestCase
+
+@end
+
+@implementation CubeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
